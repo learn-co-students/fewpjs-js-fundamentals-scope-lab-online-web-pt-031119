a48d9b252a48f2762c0371b5e922af7d42476134
Ryan <kerneltux0@gmail.com>
2019-09-27 10:43:01 -0600

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animal = ""
+let animal = "dog"
 
 function myAnimal() {
   return animal
@@ -8,12 +8,19 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  let pet = 'dog';
+  let newAnimal = pet.split('');
+  newAnimal[0]='c';
+  newAnimal[1]='a';
+  newAnimal[2]='t';
+  let animal = newAnimal.join('');
+
   return animal
 }
 
 function add2(n) {
+  let two = 2
   return n + two
 
   // Feel free to move things around!
-  two = 2
 }
